144b14519be95ae1f4066caac16e4c760ee098d3
oguchi ugochukwu <ugoebukaoguchi@gmail.com>
2023-07-25 10:01:07 +0100

    solving responsiveness issue on client page

--- client.php
+++ client.php
@@ -51,11 +51,11 @@
     <div class="row"> 
                 <div class="col-12 col-md-6 row">
                     <div class="col-2"> <h1 class="bignum text-center pt-3"> 1 </h1> </div>
-                        <div class="col-6">
+                        <div class="col-9 col-md-6">
                             <h5> FILTER </h5>
                             <p> To access our  qualified list of engineers, you need to specify a location/state. <br> It could be either the location of your (building) project or you wish to view the engineers in a particuler state. This is done using the search button above </p>
                         </div>
-                        <div class="col-4 pt-2 ic1" style="border-radius: 100%" >
+                        <div class="col d-none d-md-table-cell col-md-4 pt-2 ic1" style="border-radius: 100%" >
                             <lord-icon
                             src="https://cdn.lordicon.com/zniqnylq.json"
                             trigger="hover"
@@ -70,10 +70,10 @@
 
                 <div class="col-12 col-md-6 row"> 
                     <div class="col-2"> <h1 class="bignum text-center pt-3"> 2 </h1>  </div>
-                    <div class="col-6"> <h5> BROWSE THROUGH</h5>
+                    <div class="col-9 col-md-6"> <h5> BROWSE THROUGH</h5>
                         <p> Take a moment to browse throught the filtered results of list of engineers registered in your selected locaion/state, and find the perfect match for your project needs.</p>
                      </div>
-                        <div class="col-4 pt-2 ic1" style="border-radius: 100%" >
+                        <div class="col d-none d-md-table-cell col-md-4 pt-2 ic1" style="border-radius: 100%" >
                                 <lord-icon
                                 src="https://cdn.lordicon.com/lbsajkny.json"
                                 trigger="hover"
@@ -91,10 +91,10 @@
     <div class="row"> 
             <div class="col-12 col-md-6 row "> 
                     <div class="col-2"> <h1 class="bignum text-center pt-3"> 3 </h1>  </div>
-                    <div class="col-6"> <h5> PROFILING </h5>
+                    <div class="col-9 col-md-6"> <h5> PROFILING </h5>
                         <p> Once you have selected an engineer, you have an added feature to access their profile.
                             Herein you have access to their entire details including past works, bio-data, collborations, and a few more information.</p> </div>
-                    <div class="col-4 pt-2 ic1"style="border-radius: 100%" >
+                    <div class="col d-none d-md-table-cell col-md-4 pt-2 ic1"style="border-radius: 100%" >
                                 <lord-icon
                                 src="https://cdn.lordicon.com/gjyysfxi.json"
                                 trigger="hover"
@@ -108,9 +108,9 @@
                 
                 <div class="col-12 col-md-6 row"> 
                     <div class="col-2"> <h1 class="bignum text-center pt-3"> 4 </h1>  </div>
-                    <div class="col-6"> <h5> MAKE CONTACTS </h5>
+                    <div class="col-9 col-md-6"> <h5> MAKE CONTACTS </h5>
                         <p> Once you are certain on your choice of engineer, Make contacts with the engineer via any of the provided engineer contact information "email is best adviced".</p> </div>
-                     <div class="col-4 pt-2 ic1"style="border-radius: 100%" >
+                     <div class="col d-none d-md-table-cell col-md-4 pt-2 ic1"style="border-radius: 100%" >
                         <lord-icon
                             src="https://cdn.lordicon.com/sdhhsgeg.json"
                             trigger="hover"
@@ -126,11 +126,11 @@
         <div class="row"> 
                 <div class="col-12 col-md-6 row ">
                     <div class="col-2"> <h1 class="bignum text-center pt-3"> 5 </h1> </div>
-                        <div class="col-6">
+                        <div class="col-9 col-md-6">
                             <h5> APPOINTMENT </h5>
                             <p> Its of the norm that clients and engineers meet up to further discuss on their projects and how best to make it work, it is adviced that the meeting point should be a public place example; a bar, a cafe, a park. For security reasons</p>
                         </div>
-                        <div class="col-4 pt-2 ic1" style="border-radius: 100%" >
+                        <div class="col d-none d-md-table-cell col-md-4 pt-2 ic1" style="border-radius: 100%" >
                         <lord-icon
                             src="https://cdn.lordicon.com/mjmrmyzg.json"
                             trigger="hover"
@@ -144,10 +144,10 @@
 
                 <div class="col-12 col-md-6 row"> 
                     <div class="col-2"> <h1 class="bignum text-center pt-3"> 6 </h1>  </div>
-                    <div class="col-6"> <h5> FEEDBACKS</h5>
+                    <div class="col-9 col-md-6"> <h5> FEEDBACKS</h5>
                         <p>We value your feedback and would greatly appreciate it if you could take a moment to share your comments and rate your experience with the engineer after your projrct is completed or after you have met with them  </p>
                      </div>
-                        <div class="col-4 pt-2 ic1" style="border-radius: 100%" >
+                        <div class="col d-none d-md-table-cell col-md-4 pt-2 ic1" style="border-radius: 100%" >
                                 <lord-icon
                                 src="https://cdn.lordicon.com/whttoese.json"
                                 trigger="hover"
