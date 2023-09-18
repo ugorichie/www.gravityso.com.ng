5693e09afd62d9221bf86f5d00e257abea3e8ca1
Ugo Chuwkuebuka Oguchi <76640420+ugorichie@users.noreply.github.com>
2023-09-18 11:28:23 +0100

    Update sidebar.php

--- partials/sidebar.php
+++ partials/sidebar.php
@@ -1,12 +1,10 @@
 <div class="col d-none d-md-table-cell bg-primary">
 <div class="row" class="sidebar"> 
                 <div id="side1"> </div>
-
                 <div class="side2"> <p><a href="engineerdashboard.php" class="text-white"> <i class="fa-solid fa-house side4"></i> &nbsp; &nbsp; HOME PROFILE </a> </p> </div>
 
                 <div class="side2"> <p> <a href="editprofile.php" class="text-white"><i class="fa-solid fa-user side4"></i>&nbsp; &nbsp; EDIT PROFILE</a> </p></div>
                 
-
                 <div class="side2"> <p> <a href="engineersub.php" class="text-white"><i class="fa-solid fa-credit-card side4"></i>&nbsp; &nbsp; SUBSCRIPTION</a> </p> </div>
 
                 <div class="side2 "> <p> <a href="#" class="text-white"> <i class="fa-solid fa-calendar-day side4"></i> &nbsp; &nbsp;  POST EVENTS</a> </p> </div>
